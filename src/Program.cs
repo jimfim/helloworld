63b8013eedc063432a1dba980c12397490796e0b
James Furey <jfurey@mi9retailer.com>
2021-10-27 14:10:20 +0100

    hello world

---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace src
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while(true){
+                Console.WriteLine($"Hello World! @ {DateTime.Now}");
+                await Task.Delay(5000);
+            }
+        }
+    }
+}
